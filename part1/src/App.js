bb2c761e3b3b9507e02de67cfe7669f52c335ba3
Mahdi Qazwini <mqazwini@umich.edu>
2021-12-21 23:41:59 -0500

    Hello code

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React?
-        </a>
-      </header>
-    </div>
-  );
+function bornYear(age) {
+    const yearNow = new Date().getFullYear();
+    return yearNow - age;
+}
+
+function Hello(props) {
+    const { name, age } = props
+    const now = new Date();
+    return (
+        <div>
+            <p>Hello {name} ({age}, AKA {bornYear(props.age)})! It is now {now.toString()}.</p>
+        </div>
+    );
+}
+
+function HelloV2({name, age}) {
+    const now = new Date();
+    return (
+        <div>
+            <p>Hello {name} ({age}, AKA {bornYear(age)})! It is now {now.toString()}.</p>
+        </div>
+    );
+}
+
+function App() { 
+    console.log("nya~");
+    const age = 38;
+    return (
+        <>
+            <h1>Greetings.</h1>
+            <Hello name="Jeff" age={age} />
+            <Hello name="Joe" age={21 + 7} />
+            <HelloV2 name="Mama" age={age / 2} />
+        </>
+    );
 }
 
 export default App;
