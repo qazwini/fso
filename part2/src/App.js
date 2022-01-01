300aee7cdd3706c1470b716256c021e6091c8347
Mahdi Qazwini <mqazwini@umich.edu>
2022-01-01 02:04:34 -0500

    Finish part2

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
 
 function App() {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState("")
     const [showAll, setShowAll] = useState(true)
+    const [errorMessage, setErrorMessage] = useState(null)
 
     useEffect(() => {
         noteService
@@ -47,11 +49,17 @@ function App() {
             .then(returnedNote => {
                 setNotes(notes.map(note => note.id !== id ? note : returnedNote))
             })
+            .catch(error => {
+                setErrorMessage(`Note '${note.content}' was already removed from the server`)
+                setTimeout(() => setErrorMessage(null), 5000)
+                setNotes(notes.filter(n => n.id !== id))
+            })
     }
 
     return (
       <div>
           <h1>Notes</h1>
+          <Notification message={errorMessage} />
           <div>
               <button onClick={() => setShowAll(!showAll)}>
                   show {showAll ? "important only" : "all"}
---
+++ part2/src/components/Notification.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Notification({message}) {
+    if (message === null) {
+        return null
+    }
+
+    return (
+        <div className="error">
+            {message}
+        </div>
+    )
+}
+
+export default Notification
