d13f372652149e78dd0b86ec588c3578bff873fd
Mahdi Qazwini <mqazwini@umich.edu>
2022-01-01 04:14:51 -0500

    Fill part 3a

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,4 @@
-const http = require("http")
+const express = require("express")
 
 let notes = [
       {
@@ -21,11 +21,75 @@ let notes = [
       }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "application/json" })
-    response.end(JSON.stringify(notes))
+const app = express()
+app.use(express.json())
+
+function requestLogger(request, response, next) {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+app.use(requestLogger)
+
+
+app.get("/", (request, response) => {
+    response.send("<h1>hello world!</h1>")
 })
 
+app.get("/api/notes", (request, response) => {
+    response.json(notes)
+})
+
+app.get("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+function generateId() {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+    return maxId + 1
+}
+
+app.post("/api/notes", (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({error: "content missing"})
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
+    
+    response.json(note)
+})
+
+
+function unknownEndpoint(request, response) {
+    response.status(404).send({error: "unknown endpoint"})
+}
+app.use(unknownEndpoint)
+
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
