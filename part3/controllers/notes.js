b6294b2d415b154759969b2a7405400ff86a327c
Mahdi Qazwini <mqazwini@umich.edu>
2022-01-25 00:36:29 -0500

    Finish part 4

--- part3/controllers/notes.js
+++ part3/controllers/notes.js
@@ -1,8 +1,10 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
-    const notes = await Note.find({})
+    const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
     response.json(notes)
 })
 
@@ -15,16 +17,35 @@ notesRouter.get('/:id', async (request, response, next) => {
     }
 })
 
+function getTokenFrom(request) {
+    const auth = request.get("authorization")
+    if (auth && auth.toLowerCase().startsWith("bearer ")) {
+        return auth.substring(7)
+    }
+    return null
+}
+
 notesRouter.post('/', async (request, response, next) => {
     const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken) {
+        return response.status(401).json({ error: "token missing or invalid" })
+    }
+
+    const user = await User.findById(decodedToken.id)
 
     const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date()
+        date: new Date(),
+        user: user._id
     })
 
     const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
     response.json(savedNote)
 })
 
